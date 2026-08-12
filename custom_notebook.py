39bd58a11630b2c8bc30a1281a54f3b8d2cc1c81
SkyElysium <2817842285@qq.com>
2026-08-13 01:02:23 +0800

    改用了动态获取资源路径
    原因：在程序任务栏图标唤出右键菜单，点击运行程序，会导致资源路径错误（Pyinstaller打包问题）

---
+++ custom_notebook.py
@@ -0,0 +1,339 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import filedialog, messagebox
+
+from .config import (
+    get_settings,
+    get_path,
+    CUSTOM_X_SCROLLBAR_STYLE,
+    CUSTOM_NOTEBOOK_STYLE
+)
+from .line_number_bar import LineNumberBar
+
+
+class CustomNotebook(ttk.Notebook):
+    def __init__(self, master: 'Editor'):
+
+        super().__init__(master)
+
+        # Create the "close" button.
+        self.close_image = tk.PhotoImage(file = get_path('tab_x'))
+
+        self.custom_style = ttk.Style()
+
+        # Regard "close" as a command that lets the tab destory.
+        self.custom_style.element_create('close', 'image', self.close_image)
+
+        self.custom_style.layout('CustomNotebook', [('CustomNotebook.client', {'sticky': 'nswe'})])
+        self.custom_style.layout('CustomNotebook.Tab', CUSTOM_NOTEBOOK_STYLE)
+
+        self['style'] = 'CustomNotebook'
+
+        # X Scrollbar in each tab
+        self.custom_style.element_create("Custom.Horizontal.TScrollbar.trough", "from", "clam")
+        self.custom_style.element_create("Custom.Horizontal.TScrollbar.thumb", "from", "clam")
+        self.custom_style.element_create("Custom.Horizontal.TScrollbar.grip", "from", "clam")
+
+        self.custom_style.layout("Custom.Horizontal.TScrollbar", CUSTOM_X_SCROLLBAR_STYLE)
+
+        self.custom_style.configure("Custom.Horizontal.TScrollbar",
+            gripcount = 0,
+            background = "#c0c0c0",
+            troughcolor = '#f0f0f0',
+            bordercolor = "#f0f0f0",
+            lightcolor = '#c0c0c0',
+            darkcolor = '#c0c0c0',
+            arrowsize = 6
+        )
+
+        self.bind('<Button-1>', self._on_pressing_close)
+        self.bind('<ButtonRelease-1>', self._move_selected_tab)
+
+        self.bind('<<NotebookTabChanged>>', self.update_info_on_title)
+
+    def _on_pressing_close(self, event):
+
+        if self.identify(event.x, event.y) == 'close':
+            tab_id = f'@{event.x}, {event.y}'
+
+            self.safely_close_file(tab_id = tab_id)
+
+    def _move_selected_tab(self, event):
+
+        # Use try-except to prevent the cursor from moving on nothing.
+        try:
+            if self.identify(event.x, event.y) != 'close':
+                tab_index = self.index(f'@{event.x}, {event.y}')
+
+                self.insert(tab_index, self.get_tab()[0])
+        except tk.TclError:
+            pass
+
+    def update_info_on_title(self, event = None):
+
+        self.master.title(get_settings('win_title'))
+
+        if not self.tabs():
+            return
+
+        _, text_tab = self.get_tab()
+
+        if text_tab.path:
+            self.master.title(f'{get_settings("win_title")} - {text_tab.path}')
+
+    def safely_close_file(self, event = None, tab_id = None):
+
+        if not self.tabs():
+            return
+
+        text_tab = self.get_tab()[1] if not tab_id else self.get_tab(tab_id)
+
+        if text_tab.text_panel.edit_modified():
+            reply = messagebox.askyesnocancel(
+                title = get_settings('win_title'),
+                message = '是否在关闭之前保存文件？'
+            )
+            if reply and self.master.save_file(file_path = text_tab.path) == 'NotSaved':
+                return
+            elif reply is None:
+                return
+
+        if text_tab.path:
+            self.master.main_menu.record_new_file(text_tab.path)
+
+        self.remove_tab(tab_id = tab_id)
+
+    def add_tab(self, event = None, tab_name = '未命名'):
+
+        text_tab = TextTab(self)
+        text_tab.label = tab_name
+
+        self.add(text_tab, text = tab_name)
+
+        self.select(text_tab)
+
+        return text_tab
+
+    def remove_tab(self, event = None, tab_id = None):
+
+        text_tab = self.get_tab()[1] if not tab_id else self.get_tab(tab_id)
+
+        self.master.main_menu.font_size.trace_vdelete('w', text_tab.font_tracker)
+
+        self.forget(text_tab)
+        text_tab.destroy()
+
+        self.update_info_on_title()
+
+    def get_tab(self, which = None):
+
+        if not which:
+            tab = self.select()
+            text_tab = self.nametowidget(tab)
+
+            return tab, text_tab
+        else:
+            # All ids will be transformed to normal ids because
+            # "nametowidget" doesn't accept the format: @x, y.
+            id = self.tabs()[self.index(which)]
+            text_tab = self.nametowidget(id)
+
+            return text_tab
+
+
+class TextTab(tk.Frame):
+    def __init__(self, master: 'CustomNotebook'):
+
+        super().__init__(master)
+
+        # Tab Info
+        self.path = ''
+        self.label = ''
+
+        self.font_size = self.master.master.main_menu.font_size
+        self.font_tracker = self.font_size.trace('w', self._change_font_size)
+
+        # Interface
+        self.grid_columnconfigure(1, weight = 1)
+        self.grid_rowconfigure(0, weight = 1)
+
+        self.line_number_bar = LineNumberBar(self, self.font_size)
+        self.line_number_bar.grid(row = 0, column = 0, rowspan = 2, sticky = 'ns')
+
+        self.text_panel = TextPanel(self, self.font_size)
+        self.text_panel.grid(row = 0, column = 1, sticky = 'nsew')
+
+        self.scrollbar = tk.Scrollbar(self)
+        self.scrollbar.grid(row = 0, column = 2, sticky = 'ns')
+
+        self.x_scrollbar = ttk.Scrollbar(self, orient = 'horizontal', style = 'Custom.Horizontal.TScrollbar')
+        self.x_scrollbar.grid(row = 0, column = 1, sticky = 'sew')
+
+        self.text_panel['xscrollcommand'] = self.text_panel._is_out_of_text
+        self.text_panel['yscrollcommand'] = self.scrollbar.set
+
+        self.x_scrollbar.config(command = self.text_panel.xview)
+        self.scrollbar.config(command = self.line_number_bar.scroll)
+
+        self.line_number_bar.update_line_number()
+
+    def delay_to_update_line_number(self, event):
+
+        # The timer prevents from being called before executing "enter".
+        self.after(1, self.line_number_bar.update_line_number)
+
+    def delay_to_highlight(self, event = None):
+
+        # The timer prevents from being called before executing "click".
+        self.after(1, self.line_number_bar.update_highlight_current_line)
+
+    def _change_font_size(self, *args):
+
+        self.line_number_bar.config(font = ('Consolas', self.font_size.get()))
+        self.text_panel.config(font = ('Consolas', self.font_size.get()))
+
+
+class TextPanel(tk.Text):
+    def __init__(self, master: 'TextTab', font_size):
+
+        super().__init__(master)
+
+        self.config(
+            wrap = 'none',
+            undo = True,
+            bd = 0,
+            font = ('Consolas', font_size.get()),
+            selectbackground = '#d3e9fc',
+            selectforeground = 'black'
+        )
+
+        self.tag_config('searched', background = '#caebcb')
+        self.tag_config('selected', underline = True)
+
+        self.bind('<Button-3>', self._popup_menu)
+        self.bind('<Control-o>', self._ctrl_o)
+        self.bind('<<Modified>>', self._text_is_changed)
+
+        # For highlighting the current line
+        self.bind('<Button-1>', self.master.delay_to_highlight)
+
+        # For the line number bar
+        self.bind('<B2-Motion>', self._b2_motion)
+        self.bind('<<Selection>>', self.master.line_number_bar.scroll_when_selecting)
+        self.bind('<Any-KeyPress>', self.master.delay_to_update_line_number)
+
+        self.bind('<MouseWheel>', self.master.line_number_bar.wheel)
+
+        self._right_click_menu()
+
+    def _right_click_menu(self):
+
+        self.menu = tk.Menu(self, tearoff = False, activeforeground = 'black', activebackground = '#91c9f7')
+
+        self.menu.add_command(label = get_settings('copy'), accelerator = 'Ctrl+C', command = self.copy)
+        self.menu.add_command(label = get_settings('cut'), accelerator = 'Ctrl+X', command = self.cut)
+        self.menu.add_command(label = get_settings('paste'), accelerator = 'Ctrl+V', command = self.paste)
+        self.menu.add_separator()
+        self.menu.add_command(label = get_settings('copy_present_path'), command = self._copy_file_path)
+
+    def _popup_menu(self, event):
+
+        self.focus_set()
+        self.mark_set('insert', f'@{event.x}, {event.y}')
+
+        self.master.delay_to_highlight()
+
+        self._check_status_of_options()
+
+        self.menu.post(event.x_root, event.y_root)
+
+    def _check_status_of_options(self):
+
+        if self.master.path:
+            self.menu.entryconfig(get_settings('copy_present_path'), state = 'normal')
+        else:
+            self.menu.entryconfig(get_settings('copy_present_path'), state = 'disabled')
+
+        try:
+            self.master.master.master.clipboard_get()
+            self.menu.entryconfig(get_settings('paste'), state = 'normal')
+        except tk.TclError:
+            self.menu.entryconfig(get_settings('paste'), state = 'disabled')
+
+    def copy(self):
+
+        if not self.master.master.tabs():
+            return
+
+        self.event_generate('<<Copy>>')
+
+    def cut(self):
+
+        if not self.master.master.tabs():
+            return
+
+        self.event_generate('<<Cut>>')
+        self.master.line_number_bar.update_line_number()
+
+    def paste(self):
+
+        if not self.master.master.tabs():
+            return
+
+        self.event_generate('<<Paste>>')
+        self.master.line_number_bar.update_line_number()
+
+    def select_all(self):
+
+        if not self.master.master.tabs():
+            return
+
+        self.event_generate('<<SelectAll>>')
+
+    def undo(self):
+
+        if not self.master.master.tabs():
+            return
+
+        self.event_generate('<<Undo>>')
+        self.master.line_number_bar.update_line_number()
+
+    def redo(self):
+
+        if not self.master.master.tabs():
+            return
+
+        self.event_generate('<<Redo>>')
+        self.master.line_number_bar.update_line_number()
+
+    def _copy_file_path(self):
+
+        self.master.master.master.clipboard_clear()
+        self.master.master.master.clipboard_append(self.master.path)
+
+    def _b2_motion(self, event):
+
+        return 'break'
+
+    def _ctrl_o(self, event):
+
+        # Tkinter has bound ctrl+o inside "Text".
+        self.master.master.master.open_file()
+
+        return 'break'
+
+    def _is_out_of_text(self, upper, lower):
+
+        if self.xview() != (0.0, 1.0):
+            self.master.x_scrollbar.lift(self)
+        else:
+            self.master.x_scrollbar.lower(self)
+
+        self.master.x_scrollbar.set(upper, lower)
+
+    def _text_is_changed(self, event):
+
+        if self.edit_modified():
+            self.master.master.tab(self.master.master.get_tab()[0], text = f'*{self.master.label}')
+        else:
+            self.master.master.tab(self.master.master.get_tab()[0], text = self.master.label)
---
+++ editor.py
@@ -0,0 +1,171 @@
+import sys
+from pathlib import Path
+
+import tkinter as tk
+from tkinter import messagebox, filedialog
+
+from .config import get_settings, get_path
+from .main_menu import MainMenu
+from .custom_notebook import CustomNotebook
+
+
+class Editor(tk.Tk):
+    def __init__(self):
+
+        super().__init__()
+
+        self.title(get_settings('win_title'))
+        self.geometry(get_settings('win_size'))
+
+        self.iconphoto(True, tk.PhotoImage(file = get_path('win_icon')))
+
+        self.custom_notebook = CustomNotebook(self)
+        self.custom_notebook.pack(fill = 'both', expand = True)
+
+        self.main_menu = MainMenu(self)
+        self.config(menu = self.main_menu)
+
+        self.custom_notebook.add_tab()
+
+        bindings = {
+            ('<Control-n>', '<Control-N>')        : self.custom_notebook.add_tab,
+            ('<Control-o>', '<Control-O>')        : self.open_file,
+            ('<Control-s>', '<Control-S>')        : self.save_file,
+            ('<Control-Alt-s>', '<Control-Alt-S>'): self.save_file_as,
+            ('<Control-F4>',)                     : self.custom_notebook.safely_close_file,
+            ('<Control-f>', '<Control-F>')        : self.main_menu.popup_find_dialog,
+            ('<Control-plus>',)                   : self.main_menu.zoom_in_font,
+            ('<Control-minus>',)                  : self.main_menu.zoom_out_font
+        }
+
+        for shortcuts, method in bindings.items():
+            for shortcut in shortcuts:
+                self.bind(shortcut, method)
+
+        self.protocol('WM_DELETE_WINDOW', self.exiting)
+
+    def open_file(self, event = None, file_path = ''):
+
+        path = filedialog.askopenfilename(
+            title = '打开',
+            filetypes = [('文本文档', '*.txt'), ('所有类型', '*.*')]
+        ) if not file_path else file_path
+
+        if not path:
+            return
+
+        try:
+            file = Path(path)
+
+            if not file.exists():
+                messagebox.showwarning(
+                    title = get_settings('win_title'),
+                    message = '打开的文件路径不存在'
+                )
+                return
+
+            text = file.read_text(encoding = 'utf-8')
+
+            text_tab = self.custom_notebook.add_tab(tab_name = file.name)
+            text_tab.text_panel.insert('end', text)
+        except UnicodeDecodeError:
+            messagebox.showerror(
+                title = get_settings('win_title'),
+                message = '无法打开此文件，因为不是 UTF-8 格式，或者这是一个程序文件'
+            )
+
+            return
+
+        text_tab.path = path
+
+        text_tab.text_panel.edit_modified(False)
+
+        text_tab.text_panel.mark_set('insert', '1.0')
+        text_tab.text_panel.focus_set()
+
+        text_tab.line_number_bar.update_line_number()
+
+    def save_file(self, event = None, file_path = '') :
+
+        if not self.custom_notebook.tabs():
+            return
+
+        _, text_tab = self.custom_notebook.get_tab()
+
+        if file_path:
+            file = Path(file_path)
+        elif text_tab.path:
+            file = Path(text_tab.path)
+        else:
+            self.save_file_as()
+
+            return 'NotSaved' if not text_tab.path else None
+
+        text = text_tab.text_panel.get('1.0', 'end-1c')  # No self adding "new line".
+        file.write_text(text, encoding = 'utf-8')
+
+        text_tab.text_panel.edit_modified(False)
+
+    def save_file_as(self, event = None):
+
+        if not self.custom_notebook.tabs():
+            return
+
+        path = filedialog.asksaveasfilename(
+            title = '另存为...',
+            defaultextension = '.txt',
+            filetypes = [('文本文档', '*.txt'), ('所有类型', '*.*')]
+        )
+
+        if not path:
+            return
+
+        tab, text_tab = self.custom_notebook.get_tab()
+
+        if text_tab.path:  # When the file has been, enter.
+            self.save_file(file_path = path)
+
+            return
+
+        text_tab.path = path
+        text_tab.label = Path(path).name
+
+        self.save_file()
+
+        self.custom_notebook.tab(tab, text = text_tab.label)
+        self.custom_notebook.update_info_on_title()
+
+    def exiting(self):
+
+        if_saved = []
+
+        for tab_id in self.custom_notebook.tabs():
+            text_tab = self.custom_notebook.get_tab(tab_id)
+
+            if text_tab.path:
+                self.main_menu.record_new_file(text_tab.path)
+            if_saved.append(text_tab.text_panel.edit_modified())
+
+        if any(if_saved):
+            reply = messagebox.askyesnocancel(
+                title = get_settings('win_title'),
+                message = '存在未保存的文件，在关闭程序前手动保存所有文件？'
+            )
+            if reply or reply is None:
+                return
+
+        self._fix_win_clipboard()
+
+        sys.exit()
+
+    def _fix_win_clipboard(self):
+
+        # https://github.com/python/cpython/issues/84632
+        if sys.platform == 'win32':
+            from ctypes import windll
+
+            user32 = windll.user32
+
+            user32.OpenClipboard(0)
+            user32.GetClipboardData(1)
+            user32.CloseClipboard()
