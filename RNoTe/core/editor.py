226b649ddb9c7bdd3145b90d7771b379853c5e7a
SkyElysium <2817842285@qq.com>
2026-09-25 02:06:28 +0800

    适配翻译等

--- RNoTe/core/editor.py
+++ RNoTe/core/editor.py
@@ -90,7 +90,7 @@ class Editor(tk.Tk):
                 return
             reply = messagebox.askyesno(
                 title = get_settings('win_title'),
-                message = _(f'The file format is UTF-8. it may be {encoding}, try it?')
+                message = _('The file format is not UTF-8. it may be {encoding}, try it?').format(encoding = encoding)
             )
             if not reply:
                 return
